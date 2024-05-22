236a7cbc8755f3a218f57bdca5390e541ef9abc0
mrowaha <ashfaqrowaha@gmail.com>
2024-05-22 14:35:31 +0300

    add admin credentials for firebase

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,11 +7,13 @@ export const env = createEnv({
   },
   server: {
     FIREBASE_URL: z.string(),
+    FIREBASE_SERVICE_ACCOUNT: z.string(),
     APP_EMAIL: z.string().email(),
     APP_PASSWORD: z.string()
   },
   runtimeEnv: {
     FIREBASE_URL: process.env.FIREBASE_URL,
+    FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT,
     NEXT_PUBLIC_IMAGE_KIT_ID: process.env.NEXT_PUBLIC_IMAGE_KIT_ID,
     APP_EMAIL: process.env.APP_EMAIL,
     APP_PASSWORD: process.env.APP_PASSWORD
